780fcb68cbf309970a4737911e81d96eb6a9708a
mahala1 <77061196+mahala1@users.noreply.github.com>
2022-04-04 22:13:41 +0200

    Update TCPserver.py

--- TCPserver.py
+++ TCPserver.py
@@ -4,9 +4,8 @@ serverSocket = socket(AF_INET, SOCK_STREAM)
 serverSocket.bind(('', serverPort))
 serverSocket.listen(1)
 print 'Server is Listening for incoming Client Requests!!!'
-while True:
- connectionSocket, addr = serverSocket.accept()
- messagefromclient = connectionSocket.recv(1024)
- print 'Message from Client: ', messagefromclient
- messagefromserver = raw_input('Enter reply message for client: ')
- connectionSocket.send (messagefromserver)
+connectionSocket, addr = serverSocket.accept()
+messagefromclient = connectionSocket.recv(1024)
+print 'Message from Client: ', messagefromclient
+messagefromserver = raw_input('Enter reply message for client: ')
+connectionSocket.send (messagefromserver)
