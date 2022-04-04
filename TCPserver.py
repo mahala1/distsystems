7a6b809a9f6adb6ec82426ed7d9918c2084ddc5a
mahala1 <77061196+mahala1@users.noreply.github.com>
2022-04-04 18:28:39 +0200

    Update TCPserver.py

--- TCPserver.py
+++ TCPserver.py
@@ -1,1 +1,12 @@
- 
+from socket import *
+serverPort = 12000
+serverSocket = socket(AF_INET, SOCK_STREAM)
+serverSocket.bind(('', serverPort))
+serverSocket.listen(1)
+print 'Server is Listening for incoming Client Requests!!!'
+while True:
+ connectionSocket, addr = serverSocket.accept()
+ messagefromclient = connectionSocket.recv(1024)
+ print 'Message from Client: ', messagefromclient
+ messagefromserver = raw_input('Enter reply message for client: ')
+ connectionSocket.send (messagefromserver)
